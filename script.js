7233700bde554790595c62cdf69f3cf4cfc3fed1
Aaron Wolf <aaron.wolf@gmail.com>
2026-06-21 10:14:41 +0300

    added bigfoot

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 "use strict";
 console.log('testing');
 console.log("Don't leave console logs in production code!");
+const heading = document.getElementById('heading');
+// @ts-ignore
+heading?.innerText = 'Bigfoot was here';
